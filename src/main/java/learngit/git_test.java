6247bd3769fc91656f0957f405cc4a4f3ef8462c
XLF192519 <73683759+XLF192519@users.noreply.github.com>
2022-05-31 11:27:20 +0800

    Update git_test.java

--- src/main/java/learngit/git_test.java
+++ src/main/java/learngit/git_test.java
@@ -9,5 +9,6 @@ public class git_test {
         System.out.println("也是冲突合并");
         System.out.println("冲突合并");
         System.out.println("练习一下push");
+        System.out.println("在远程库添加一行，练习一下pull");
     }
 }
