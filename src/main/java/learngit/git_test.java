e0312a1ffd3f6683a20a825a8a079f032769f379
XLF192519 <1206971855@qq.com>
2022-05-31 10:38:02 +0800

    third commit

--- src/main/java/learngit/git_test.java
+++ src/main/java/learngit/git_test.java
@@ -4,5 +4,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("123456");
         System.out.println("123456");
+        System.out.println("123456");
     }
 }
